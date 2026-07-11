24ec7356946ddbef23fbd138f1f8211d5eb4541e
Pedro Henrique <pedrohenriqueb366@gmail.com>
2026-07-11 11:21:32 -0300

    feat(client): set up Tailwind design tokens and app shell

---
+++ client/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TrailerPlayer from '@/components/player/TrailerPlayer.vue'
+import ToastNotification from '@/components/ui/ToastNotification.vue'
+</script>
+
+<template>
+  <RouterView />
+  <TrailerPlayer />
+  <ToastNotification />
+</template>
